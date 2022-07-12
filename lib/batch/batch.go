c5ed41c6d35a788658958c0cac1d88fb179857db
artemiy.krot <artemiy.krot@gmail.com>
2022-07-12 16:07:16 +0300

    first attempt

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -14,5 +15,26 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	var wg sync.WaitGroup
+	semaphore := make(chan struct{}, pool)
+	var mx sync.Mutex
+	var i int64
+	for i = 0; i < n; i++ {
+
+		wg.Add(1)
+		semaphore <- struct{}{}
+		go func(j int64) {
+			userGottenFromDb := getOne(j)
+			mx.Lock()
+			res = append(res, userGottenFromDb)
+			mx.Unlock()
+			<-semaphore
+			wg.Done()
+		}(i)
+
+	}
+
+	wg.Wait()
+
+	return
 }
